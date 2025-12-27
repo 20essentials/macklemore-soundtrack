1cdd27ce16cd16d6602a634d00421f79bb754da7
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:31:05 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MACKLEMORE';
+export let nameSongs = [
+	"CAN'T HOLD US ", /*1*/
+	"THRIFT SHOP", /*2*/
+	"CAN'T HOLD US - FORMULA 1 VER.", /*3*/
+	"SAME LOVE ", /*4*/
+	"GLORIOUS ", /*5*/
+	"GOOD OLD DAYS", /*6*/
+	"WHITE WALLS ", /*7*/
+	"DOWNTOWN ", /*8*/
+	"WINGS", /*9*/
+	"AND WE DANCED", /*10*/
+	"MY OH MY", /*11*/
+	"GROWING UP", /*12*/
+	"IRISH CELEBRATION", /*13*/
+	"VICTORY LAP", /*14*/
+	"STARTING OVER", /*15*/
+	"DRUG DEALER ", /*16*/
+	"NEON CATHEDRAL ", /*17*/
+	"TEN THOUSAND HOURS", /*18*/
+	"BUCKSHOT ", /*19*/
+	"THE TRAIN", /*20*/
+]
